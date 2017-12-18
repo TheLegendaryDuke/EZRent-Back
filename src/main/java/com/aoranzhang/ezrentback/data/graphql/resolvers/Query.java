aefefdd4e1fba909ecf3f83e534f267be52a013c
Aaron <454048573@qq.com>
2017-12-18 18:32:08 -0500

    graphql schema change

--- src/main/java/com/aoranzhang/ezrentback/data/graphql/resolvers/Query.java
+++ src/main/java/com/aoranzhang/ezrentback/data/graphql/resolvers/Query.java
@@ -1,17 +1,28 @@
 package com.aoranzhang.ezrentback.data.graphql.resolvers;
 
+import com.aoranzhang.ezrentback.data.entity.City;
 import com.aoranzhang.ezrentback.data.entity.User;
+import com.aoranzhang.ezrentback.service.CityService;
 import com.aoranzhang.ezrentback.service.UserService;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Set;
+
 @Component
 public class Query implements GraphQLQueryResolver {
-    @Autowired
-    private UserService userService;
+//    @Autowired
+//    private UserService userService;
 
-    public User user(String id) {
-        return userService.getUserById(id);
+    @Autowired
+    private CityService cityService;
+
+//    public User user(String id) {
+//        return userService.getUserById(id);
+//    }
+
+    public Set<City> cities(){
+        return cityService.getAllCities();
     }
 }
