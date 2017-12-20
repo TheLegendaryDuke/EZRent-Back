cb1441f060381fc38630148af5d4ced393e8b744
TheLegendaryDuke <454048573@qq.com>
2017-12-20 13:56:59 -0500

    graphql schema update

--- src/main/java/com/aoranzhang/ezrentback/data/graphql/resolvers/Query.java
+++ src/main/java/com/aoranzhang/ezrentback/data/graphql/resolvers/Query.java
@@ -1,17 +1,40 @@
 package com.aoranzhang.ezrentback.data.graphql.resolvers;
 
+import com.aoranzhang.ezrentback.data.entity.City;
 import com.aoranzhang.ezrentback.data.entity.User;
+import com.aoranzhang.ezrentback.service.CityService;
 import com.aoranzhang.ezrentback.service.UserService;
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpSession;
+import java.util.Set;
 
 @Component
 public class Query implements GraphQLQueryResolver {
     @Autowired
+    private HttpSession httpSession;
+
+    @Autowired
     private UserService userService;
 
-    public User user(String id) {
-        return userService.getUserById(id);
+    @Autowired
+    private CityService cityService;
+
+    public User user() {
+        String user = (String) httpSession.getAttribute("userEmail");
+        if(user != null && user.length() != 0) {
+            return userService.getUserByEmail(user);
+        }
+        return null;
     }
+
+    public Set<City> cities() {
+        return cityService.getAllCities();
+    }
+
 }
